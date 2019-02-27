baaa7579412d459867823d8cb6ca10ed252249bb
danebell <dane@email.arizona.edu>
2019-02-27 11:50:17 -0800

    Start test structure

---
+++ core/src/test/scala/ai/lum/odinson/core/TestUtils.scala
@@ -0,0 +1,21 @@
+package ai.lum.odinson.core
+
+import java.io.File
+import java.nio.file.Path
+
+import ai.lum.odinson.ExtractorEngine
+import com.typesafe.config.ConfigFactory
+
+object TestUtils {
+  val config             = ConfigFactory.load()
+  val indexDir           = config[Path]("odinson.indexDir")
+
+  val docIdField         = config[String]("odinson.index.documentIdField")
+  val sentenceIdField    = config[String]("odinson.index.sentenceIdField")
+  val wordTokenField     = config[String]("odinson.index.wordTokenField")
+
+  val vocabFile          = config[File]("odinson.compiler.dependenciesVocabulary")
+
+  val extractorEngine = new ExtractorEngine(indexDir)
+
+}
