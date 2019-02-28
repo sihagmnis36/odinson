d8eb1d6c739cb147428cd766125c13372ff5ecf2
danebell <dane@email.arizona.edu>
2019-02-28 12:57:12 -0800

    First tests

--- core/src/test/scala/ai/lum/odinson/core/TestQueries.scala
+++ core/src/test/scala/ai/lum/odinson/core/TestQueries.scala
@@ -8,7 +8,36 @@ class TestQueries extends FlatSpec with Matchers {
     extractorEngine.numDocs() should be (477) // sentences, not papers
   }
 
-  it should "" in {
-
+  it should "retrieve docs by ID" in {
+    extractorEngine.doc(337)
+    // FIXME: working .getParentDoc
+    //extractorEngine.getParentDoc("10.1093/nar/gkt837")
   }
+
+  it should "retrieve basic keyword query" in {
+    val res = extractorEngine.query("phosphorylation")
+    res.totalHits should be (162)
+    res.maxScore should not equal 0.0
+    res.scoreDocs.foreach(d => d.matches should not be empty)
+    res.scoreDocs.foreach(d => d.matches.foreach{_.captures should be (empty)})
+  }
+
+  it should "only retrieve n documents" in {
+    val res = extractorEngine.query("phosphorylation", 2)
+    res.totalHits should be (162)
+    res.scoreDocs.length should be (2)
+    res.maxScore should not equal 0.0
+    res.scoreDocs.foreach(d => d.matches should not be empty)
+    res.scoreDocs.foreach(d => d.matches.foreach{_.captures should be (empty)})
+  }
+
+  it should "limit search by parent-doc query" in {
+    val res = extractorEngine.query("phosphorylation", "title:Proteomic")
+    res.scoreDocs.foreach{d =>
+      extractorEngine.doc(d.doc).getField("docId").stringValue() should be ("10.1186/1559-0275-10-1")
+    }
+    extractorEngine.query("phosphorylation", "title:Proteomic", 1).scoreDocs.length should be (1)
+  }
+
+
 }
--- core/src/test/scala/ai/lum/odinson/core/TestUtils.scala
+++ core/src/test/scala/ai/lum/odinson/core/TestUtils.scala
@@ -1,6 +1,5 @@
 package ai.lum.odinson.core
 
-import java.io.File
 import java.nio.file.Path
 
 import ai.lum.odinson.ExtractorEngine
@@ -11,13 +10,5 @@ object TestUtils {
   val config             = ConfigFactory.load()
   val indexDir           = config[Path]("odinson.indexDir")
 
-  val docIdField         = config[String]("odinson.index.documentIdField")
-  val sentenceIdField    = config[String]("odinson.index.sentenceIdField")
-  val wordTokenField     = config[String]("odinson.index.wordTokenField")
-
-  val vocabFile          = config[File]("odinson.compiler.dependenciesVocabulary")
-
   val extractorEngine = new ExtractorEngine(indexDir)
-
-  println("ExtractorEngine loaded w/o asplodin'")
 }
