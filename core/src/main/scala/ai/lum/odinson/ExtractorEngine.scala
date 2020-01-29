aaa86391122db99c4adc7d451157e276c284ac87
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2020-01-28 21:15:13 -0800

    maybeQuote

--- core/src/main/scala/ai/lum/odinson/ExtractorEngine.scala
+++ core/src/main/scala/ai/lum/odinson/ExtractorEngine.scala
@@ -17,6 +17,7 @@ import ai.lum.odinson.lucene.analysis.TokenStreamUtils
 import ai.lum.odinson.lucene.search._
 import ai.lum.odinson.state.State
 import ai.lum.odinson.digraph.Vocabulary
+import ai.lum.odinson.utils.QueryUtils._
 
 
 
@@ -84,18 +85,6 @@ class ExtractorEngine(
         Some(explanation)
       case _ => None
     }
-  }
-
-  // FIXME move this method to somewhere else
-  // utils maybe?
-  def maybeQuoteWord(s: String): String = {
-    val isValidIdentifier = "^[a-zA-Z_][a-zA-Z0-9_]*$".r.findFirstIn(s).isDefined
-    if (isValidIdentifier) s else "\"" + s.escapeJava + "\""
-  }
-
-  def maybeQuoteLabel(s: String): String = {
-    val isValidIdentifier = "^[a-zA-Z_][a-zA-Z0-9_:-]*$".r.findFirstIn(s).isDefined
-    if (isValidIdentifier) s else "\"" + s.escapeJava + "\""
   }
 
   /** executes query and returns all results */
